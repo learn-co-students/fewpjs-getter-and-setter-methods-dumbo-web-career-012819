75272aa67e5380139e7080cdd83b29c2966990e2
mazenswar <mazenswar@protonmail.com>
2019-03-18 07:10:18 -0400

    set and get

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your Circle class here
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  get diameter() {
+    return this.radius * 2;
+  }
+
+  get circumference() {
+      return Math.PI * this.diameter;
+  }
+
+  get area() {
+    return Math.PI * (this.radius ** 2);
+  }
+
+  set diameter(newDiameter) {
+    this.radius = newDiameter / 2;
+
+  }
+
+  set circumference(newCircumference) {
+    this.radius = (newCircumference / Math.PI) / 2;
+  }
+
+  set area(newArea) {
+    this.radius = Math.sqrt(newArea / Math.PI);
+  }
+//
+}
+
+// Define setter methods for diameter, circumference, and area which will accept values for each calculation, calculate the radius based on the input value and set this.radius accordingly
